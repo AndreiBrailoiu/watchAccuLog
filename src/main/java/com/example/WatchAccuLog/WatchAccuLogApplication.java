201b232c294670623a7aecf0af3bb79416c0cad2
AndreiBrailoiu <darkyss94@gmail.com>
2022-11-24 11:42:07 +0200

    removed static creation of users

--- src/main/java/com/example/WatchAccuLog/WatchAccuLogApplication.java
+++ src/main/java/com/example/WatchAccuLog/WatchAccuLogApplication.java
@@ -58,11 +58,11 @@ public class WatchAccuLogApplication {
 			}
 
 			// user/user and admin/admin profile creation
-			urepository.save(
+			/*urepository.save(
 					new UserEntity("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER"));
 			urepository.save(
 					new UserEntity("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN"));
-
+					*/
 			log.info("Fetching all added watches");
 			for (Watch watch : repository.findAll()) {
 				log.info(watch.toString());
